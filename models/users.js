c90d83db9f18504f471359931a19dc6b6936501c
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-09-20 20:06:17 +0530

    USERs Model

---
+++ models/users.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email address"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password for your account"],
+    minlength: [8, "Your password must be at least 8 characters long"],
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: {
+      values: ["user", "employeer"],
+      message: "Please select correct role",
+    },
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+module.exports = mongoose.model("User", userSchema);
