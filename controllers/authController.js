bc4e8fc9e326b9ea3cf22c269b53fddfd2548f0f
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-09-21 00:04:52 +0530

    Login User and assign Token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/users");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const ErrorHandler = require("../utils/errorHandler");
 
 // Register a new user   =>   /api/v1/register
 
@@ -19,6 +20,38 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     success: true,
     message: "User is registered.",
     // data: user,
+    token,
+  });
+});
+
+// Login user  =>  /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Checks if email or password is entered by user
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & Password"), 400);
+  }
+
+  // Finding user in database
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password.", 401));
+  }
+
+  // Check if password is correct
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+
+  //Create JSON Web tokem
+  const token = user.getJwtToken();
+
+  res.status(200).json({
+    success: false,
     token,
   });
 });
